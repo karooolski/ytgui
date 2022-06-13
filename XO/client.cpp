e70c1744ce9d1033ef199d17cfde858fe542ca06
karooolski <106780820+karooolski@users.noreply.github.com>
2022-06-13 17:31:05 +0200

    wysylanie charow na serwer
    kompilancja
    g++ client.cpp -o client
    odpalanie:
    ./client
    oczywiście serwer musi być uruchomiony, 
    jako input dajesz pojedynczy char
    na serwerze pojawiają się chary storowane w wektorze które wysłałeś z klienta konsoli

--- XO/client.cpp
+++ XO/client.cpp
@@ -9,183 +9,7 @@
 #define PORT 8080
 using namespace std;
 
-void show(char *XO){
-	
-	for(int i=0; i<9; i++){
-		if(i%3==0) cout << "\n\n" ;//<< "------"<<endl;
-		cout << i << "\t";
-	}
-	cout << "\n\n";
 
-	for(int i=0; i<9; i++){
-		if(i%3==0) cout << "\n\n" ;//<< "------"<<endl;
-		cout << (char)XO[i] << "\t";
-	}
-}
-
-bool no_winer(char *XO){
-	for(int i = 0 ; i < 9 ; i++){
-		if(XO[i]=='0')
-			return false;
-	}
-	return true;
-}
-
-// 0 1 2 
-// 3 4 5 
-// 6 7 8 
-// sprawdzanie kto wygral
-bool check(char *XO){
-	// poziomo gora
-	if(XO[0]==XO[1] && XO[1]==XO[2]){
-		if(XO[0]!='0'&&XO[1]!='0'&&XO[2]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[0] << " Winned the game(1)!";
-			return true;
-		}
-	}
-	// poziomo srodek
-	if(XO[3]==XO[4] && XO[1]==XO[5]){
-		if(XO[3]!='0'&&XO[4]!='0'&&XO[5]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[3] << " Winned the game!(2)";
-			return true;
-		}
-	}
-	// poziomo dol
-	if(XO[6]==XO[7] && XO[7]==XO[8]){
-		if(XO[6]!='0'&&XO[7]!='0'&&XO[8]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[6] << " Winned the game!(3)";
-			return true;
-		}
-	}
-	// skos od lewej 
-	if(XO[0]==XO[4] && XO[4]==XO[8]){
-		if(XO[0]!='0'&&XO[4]!='0'&&XO[8]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[0] << " Winned the game!(4)";
-			return true;
-		}
-	}
-	// skos od prawej
-	if(XO[2]==XO[4] && XO[4]==XO[6]){
-		if(XO[2]!='0'&&XO[4]!='0'&&XO[6]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[2] << " Winned the game!(5)";
-			return true;
-		}
-	}
-	// skos pion 1 
-	if(XO[0]==XO[3] && XO[3]==XO[6]){
-		if(XO[0]!='0'&&XO[3]!='0'&&XO[6]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[0] << " Winned the game!(6)";
-			return true;
-		}
-	}
-	// skos pion 2
-	if(XO[1]==XO[4] && XO[4]==XO[7]){
-		if(XO[1]!='0'&&XO[4]!='0'&&XO[7]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[1] << " Winned the game!(7)";
-			return true;
-		}
-	}
-			// skos pion 2
-	if(XO[2]==XO[5] && XO[5]==XO[8]){
-		if(XO[2]!='0'&&XO[5]!='0'&&XO[8]!='0'){
-			show(&XO[0]);
-			cout << "\n" << (char)XO[2] << " Winned the game!(8)";
-			return true;
-		}
-	}
-	if(no_winer(&XO[0])){
-		show(&XO[0]);
-		cout << "\n" << " No one winned the game!";
-		return true;
-	}
-	return false;
-}
-
-char switcher(char player){
-	if(player=='X') return 'Y';
-	if(player=='Y') return 'X';
-	else {cout << "\n switcher: blad!\n"; return 'S'; }
-}
-
-// sprawdzenie poprawnosci danych dla pojedynczych przypadkow
-void check_test(){
-	char XO[9];
-	XO[0] = 'X'; XO[1] = '0'; XO[2] = '0';
-	XO[3] = 'X'; XO[4] = '0'; XO[5] = '0';
-	XO[6] = 'X'; XO[7] = '0'; XO[8] = '0';
-	show(&XO[0]);
-	check(&XO[0]);
-	XO[0] = '0'; XO[1] = 'X'; XO[2] = '0';
-	XO[3] = '0'; XO[4] = 'X'; XO[5] = '0';
-	XO[6] = '0'; XO[7] = 'X'; XO[8] = '0';
-	show(&XO[0]);
-	check(&XO[0]);
-	XO[0] = '0'; XO[1] = '0'; XO[2] = 'X';
-	XO[3] = '0'; XO[4] = '0'; XO[5] = 'X';
-	XO[6] = '0'; XO[7] = '0'; XO[8] = 'X';
-	show(&XO[0]);
-	check(&XO[0]);
-	XO[0] = 'X'; XO[1] = '0'; XO[2] = '0';
-	XO[3] = '0'; XO[4] = 'X'; XO[5] = '0';
-	XO[6] = '0'; XO[7] = '0'; XO[8] = 'X';
-	show(&XO[0]);
-	check(&XO[0]);
-	XO[0] = 'X'; XO[1] = 'Y'; XO[2] = 'Y';
-	XO[3] = 'Y'; XO[4] = 'X'; XO[5] = 'X';
-	XO[6] = 'X'; XO[7] = 'Y'; XO[8] = 'Y';
-	show(&XO[0]);
-	check(&XO[0]);
-}
-
-
-void game(){
-	//check_test();
-	//return 0;
-	
-	char XO[9];
-	for(int i=0; i<9; i++)
-	XO[i] = '0';
-	XO[0] = '0'; XO[1] = '0'; XO[2] = '0';
-	XO[3] = '0'; XO[4] = '0'; XO[5] = '0';
-	XO[6] = '0'; XO[7] = '0'; XO[8] = '0';
-	
-	show(&XO[0]);
-
-	char player;
-	player = 'X';
-
-	cout << endl;
-
-	// logika gry
-	while(!(check(&XO[0]))){ // dopoki nie ma wygranej albo /przegranej dla obu stron
-		int wybierz_pole ; 
-		bool stop = true;
-		while(stop){ // dziala dopoki nie ustawisz pola
-			show(&XO[0]);
-			cout << player <<" "; cin >> wybierz_pole; // wybor pola
-			if(wybierz_pole>-1 && wybierz_pole<9){
-				if(XO[wybierz_pole]=='0'){
-					XO[wybierz_pole] = player; // wpis pola
-					cout << "ustawiono pole\n";
-					player = switcher(player); // zamiana X na Y, Y na X
-					stop = false;
-				}else{
-					cout << "\n to pole zostalo juz wybrane! \n";
-				}
-			}else{
-				cout << "\n nieprawidlowe pole \n";
-			}
-		}
-	}
-
-}
 
 int first_connection(){
 
@@ -193,10 +17,11 @@ int first_connection(){
 	//out << endl << endl;
 	
 	// logika Client - server
-	while(true){
+	bool flag_f = true;
+	while(flag_f){
 		int sock = 0, valread, client_fd;
 		struct sockaddr_in serv_addr;
-		char* hello = "Hello from client Madry";
+		char* hello = "Gracz: dzien dobry";
 		char buffer[1024] = { 0 };
 		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 			printf("\n Socket creation error \n");
@@ -217,19 +42,24 @@ int first_connection(){
 			printf("\nConnection Failed \n");
 			return -1;
 		}
-
-		while(true){ // moj while
-			send(sock, hello, strlen(hello), 0);
-			printf("Hello message from client sent to server\n");
-			valread = read(sock, buffer, 1024);
+		bool flag = true;
+		while(flag){ // moj while
+			//send(sock, hello, strlen(hello), 0);
+			//printf("Hello message from client sent to server\n");
+			//valread = read(sock, buffer, 2024);
 			printf("%s\n", buffer);
-			int a;
-			cin>>a;
-			char b = a + '0';
+			//int a;
+			//cin>>a;
+			//char b = a + '0';
+			char b;
+			cin >> b;
 			char *y = &b;
-
-			valread = read(sock,buffer, 1024);
-			send(sock, y, strlen(y), 0);
+			cout <<" wsyweitlam char " << y << "\n";
+			flag = false; 
+			flag_f=false;
+			send(sock, &b, strlen(y), 0);
+			valread = read(sock,buffer, 2024);
+			break;
 		}
 	// closing the connected socket
 	close(client_fd);
