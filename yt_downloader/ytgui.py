476b5e73cbeb7fec99dc079b228f9df372ff0ccb
karooolski <106780820+karooolski@users.noreply.github.com>
2022-07-20 15:43:25 +0200

    Works on windows

--- yt_downloader/ytgui.py
+++ yt_downloader/ytgui.py
@@ -1,7 +1,18 @@
-import tkinter as tk
+import tkinter as tk # 20.07.2022
 from pytube import YouTube 
 #https://www.geeksforgeeks.org/how-to-get-the-input-from-tkinter-text-box/
-  
+
+# this program also need '/' slashes in the path like in linux instead "\"
+def change_backslashes(word):
+    ex = '\\'
+    l = list(word)
+    for i in range(len(word)):
+        #w = print(word[i])
+        if l[i] == ex:
+            l[i] = '/'
+        s = ''.join(l)
+    return(s)
+
 # Top level window
 frame = tk.Tk()
 frame.title("TextBox Input")
@@ -11,7 +22,9 @@ frame.geometry('500x300')
 #path_confirm = 0
 
 global SAVE_PATH
-global link
+SAVE_PATH = ""
+global link 
+link = ""
 
 def printInput():
    
@@ -20,6 +33,7 @@ def printInput():
     inp = inputtxt.get(1.0, "end-1c")
     global SAVE_PATH
     SAVE_PATH = inp
+    SAVE_PATH = change_backslashes(SAVE_PATH)
     l = lbl.config(text = "Provided Input: "+inp)
     print(inp)
     
