4143e62e9c3cd6e1e7ffaa3f8975a008c0fea3ab
karooolski <106780820+karooolski@users.noreply.github.com>
2022-07-20 17:59:47 +0200

    Many changes
    - Added Lock to a download button when the path is not setted 
    - Added progressbar in console, I tried to do in the GUI but this first one works the best for now
    - added grey background and white text -> information

--- yt_downloader/ytgui.py
+++ yt_downloader/ytgui.py
@@ -1,6 +1,28 @@
 import tkinter
+from tkinter import ttk
+from turtle import color
 from pytube import YouTube 
+from pytube.cli import on_progress #this module contains the built in progress bar. 
+import time
 #https://www.geeksforgeeks.org/how-to-get-the-input-from-tkinter-text-box/
+
+
+
+# def step():
+#     for i in range(5):
+#         ws.update_idletasks()
+#         pb1['value'] += 20
+#         time.sleep(1)
+
+#def temp_text(e):
+#   textBoxPath.delete(0,"end")
+
+ASCI_grey = "#808080"
+TEXT_collor = "white"
+
+def enableDownloadButton():
+    if printButtonDownload["state"] == "disabled":
+        printButtonDownload["state"] = "normal" #active
 
 # this program also need '/' slashes in the path like in linux instead "\"
 def change_backslashes(word):
@@ -15,8 +37,9 @@ def change_backslashes(word):
 
 # Top level window
 frame = tkinter.Tk()
-frame.title("TextBox Input")
+frame.title("YouTube Video Downloader")
 frame.geometry('500x300')
+frame.configure(background=ASCI_grey)
 
 global SAVE_PATH
 SAVE_PATH = ""
@@ -30,6 +53,7 @@ def buttonActionConfirmThePath():
     SAVE_PATH = change_backslashes(SAVE_PATH)
     labelPath.config(text = "Provided Input: "+input)
     print(input)
+    enableDownloadButton()
     
 def buttonActionDownload():
     input = textBoxDownload.get(1.0, "end-1c")
@@ -41,35 +65,51 @@ def buttonActionDownload():
     print("Try download a video")
     print("link: "+link)
     print("SAVE_PATH: "+SAVE_PATH)
-
     try: 
-        yt = YouTube(link) 
+        yt = YouTube(link,on_progress_callback=on_progress) 
     except: 
         print("YouTube(link) error: Connection Failed") #to handle exception 
+    #   pb.start()
+    #   step()
     try: 
         yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(SAVE_PATH)
     except: 
         print("yt.streams.filter error!\n Do you set the PATH correctly?") 
+    #pb.stop()
     print('Finished working!') 
   
 # TextBox Creation
 textBoxPath = tkinter.Text(frame,height = 5,width = 20)
+#textbox = Entry(win, bg="white", width=50, borderwidth=2)
+#textBoxPath.insert(0,"Ener here a PATH")
 textBoxPath.pack()
 # Button Creation
-printButton = tkinter.Button(frame,text = "confirm the PATH", command = buttonActionConfirmThePath)
-printButton.pack()
+printButtonPath = tkinter.Button(frame,text = "confirm the PATH", command = buttonActionConfirmThePath)
+printButtonPath.pack()
 # Label Creation
-labelPath = tkinter.Label(frame, text = "")
+labelPath = tkinter.Label(frame, text = "Enter here^ a PATH where to download:^",background=ASCI_grey,fg=TEXT_collor)
 labelPath.pack()
+#textBoxPath.bind("<FocusIn>", temp_text)
+
 
 textBoxDownload = tkinter.Text(frame,height = 5,width = 20)  
 textBoxDownload.pack()
-printButton2 = tkinter.Button(frame,text = "confirm link and download", command = buttonActionDownload)
-printButton2.pack()
-label_download = tkinter.Label(frame,text="")
+printButtonDownload = tkinter.Button(frame,text = "confirm link and download", command = buttonActionDownload)
+printButtonDownload.pack()
+printButtonDownload["state"] = "disabled"  # button is disabled at start 
+label_download = tkinter.Label(frame,text="Enter here a YouTube Link ^", background=ASCI_grey,fg=TEXT_collor)
 label_download.pack()
+#frame.grid()
 
-
+# progressbar
+# pb = ttk.Progressbar(
+#     frame,
+#     orient='horizontal',
+#     mode='indeterminate',
+#     length=280
+# )
+# place the progressbar
+#pb.pack()
 
 frame.mainloop()
 
