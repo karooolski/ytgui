d76cb822521458c68dd80b7e8ebfdd1948a05fc8
karooolski <106780820+karooolski@users.noreply.github.com>
2022-07-21 19:47:34 +0200

    Dowload videos from YouTube in High Quality

---
+++ yt_downloader/film_playlist_HQ.py
@@ -0,0 +1,24 @@
+import pytube #21.07.2022
+from pytube import YouTube
+from pytube.cli import on_progress
+# if you are running it in windows, remember to change '\' to '/' in the path,
+# because '\' in string has its own function - makes new line
+# for example: good one: C:/Users/<Username>/Desktop/ytgui/audioplaylisttest
+SAVE_PATH = ""
+link = ''
+
+try:
+    playlist = pytube.Playlist(link)
+    count_ = 1
+    total = str(playlist.length)
+    for video in playlist.videos:
+        try:
+            print("downloading ("+str(count_)+"/"+str(total)+") "+video.title)
+            video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(SAVE_PATH)
+            count_ += 1
+        except: 
+            print("some problem occured during dowloading!")
+except: 
+    print("connection with playlist failed")
+
+
